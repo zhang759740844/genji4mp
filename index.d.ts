f9a81561d2b1834f6037feba1c5bc34e52acd40e
zhang759740844 <759740844@qq.com>
2019-07-15 16:52:33 +0800

    add ts的声明文件

---
+++ index.d.ts
@@ -0,0 +1,23 @@
+declare class BasePage {
+  constructor ()
+  mixinLifeCycle (funcObjc: any): any
+  mixinAction (funcObjc: any): any
+  register (props: any, data: any, lifeCycle: any, privateMethod: any, viewAction: any, computed?: any, watch?: any): any
+}
+
+declare class BaseService {
+  constructor ()
+  registerEvent (eventName: string, callback: (data: any) => void): void
+  resignEvent (eventName: string): void
+  executeEvent (eventName: string, data: any): void
+  reLaunch (baseUrl: string, params: any): void
+  navigateTo (baseUrl: string, params: any): void
+  redirectTo (baseUrl: string, params: any): void
+  switchTab (baseUrl: string): void
+  navigateBack (delta?: number, data?: any, hintString?: string, icon?: string): void
+  registerRouter(routers: Array<string>, moduleName?: string): void
+}
+
+export let $Page: BasePage
+export let $wx: BaseService
+export let settinghelper: (funcName: string, info?: string, authorizeLevel?: number, data?: object) => Promise<void>
